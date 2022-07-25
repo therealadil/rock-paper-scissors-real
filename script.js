71906dcbbba98fd0b4b518d65580416b63e84d01
AK <adilkisthebest@gmail.com>
2022-07-25 14:33:20 +0100

    add logic

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-
-
-
-
 function getComputerChoice  () {
 
     const gameResults = ["rock", "paper", "scissors"];
@@ -11,3 +7,48 @@ function getComputerChoice  () {
     return gameResults[randomResult];
 }
 
+function askPlayer () {
+    const playerChoice = (prompt("Rock, Paper, or Scissors?")).toLowerCase();
+    
+    return playerChoice;
+
+
+}
+
+
+
+function playRound (playerSelection, computerSelection) {
+
+    
+
+    if (playerSelection == computerSelection) {
+        return "You picked " + playerSelection + ". They picked " + computerSelection + ". It's a tie." 
+    } else if (
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper")
+        ) {
+            return "You picked " + playerSelection + ". They picked " + computerSelection + ". You win." 
+        } else if (
+            (playerSelection == "scissors" && computerSelection == "rock") ||
+            (playerSelection == "rock" && computerSelection == "paper") ||
+            (playerSelection == "paper" && computerSelection == "scissors")
+        ) {
+            return "You picked " + playerSelection + ". They picked " + computerSelection + ". You lose." 
+        } else {
+            alert("this is invalid entry");
+            console.log(playRound(askPlayer(), getComputerChoice()));
+        }
+
+
+}
+
+
+
+
+    function game () {
+        for (let i = 0; i<5; i++) {
+            playRound( askPlayer(), getComputerChoice() )
+
+        }
+    }
